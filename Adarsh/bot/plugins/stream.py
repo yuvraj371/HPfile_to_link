28f20bb81514da462ef368f098c011d0ddc35554
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-20 14:23:08 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -24,7 +24,7 @@ pass_db = Database(Var.DATABASE_URL, "ag_passwords")
 async def login_handler(c: Client, m: Message):
     try:
         try:
-            ag = await m.reply_text("Now send me password.\n\n for password send 10rs to `hp148981@paytm` and send screen shot to @hpsupportbot \n\n (for running bot need contributions..unwanted message = ban + report)")
+            ag = await m.reply_text("Now send me password.\n\n for password send 10rs to `krishna527062@oksbi` and send screen shot to @Madhuri_niranjan \n\n (for running bot need contributions..unwanted message = ban + report)")
             _text = await c.listen(m.chat.id, filters=filters.text, timeout=90)
             if _text.text:
                 textp = _text.text
