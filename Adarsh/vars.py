4ca3e1ebffd3b7e079a1bd592f44742a85ec1304
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-30 12:29:50 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,4 +1,3 @@
-# (c) harshil8981 
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -9,32 +8,32 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', '21938068'))
+    API_HASH = str(getenv('API_HASH', 'c18fd98f3e58484df0aecd95a3d5a6a9'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5972634422:AAG2Q7ozpB4tKCVTCTJcFT7504GpeUYNTgM'))
     name = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001730461072'))
+    PORT = int(getenv('PORT', 65535))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '181.214.152.133'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5787509903").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Madhuri_niranjan'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '181.214.152.133:65535')) if not ON_HEROKU or getenv('FQDN', '181.214.152.133:65535') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Filetolink:yuvraj178@cluster0.focy2ua.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Infinity_XBotz'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
