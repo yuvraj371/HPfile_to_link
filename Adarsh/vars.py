6c0e2803b4da56f4db1590b446dc681d0d0e0ad7
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-30 12:30:50 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -2,45 +2,38 @@ import os
 from os import getenv, environ
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
 class Var(object):
-    # Basic settings
-    API_ID = int(getenv('API_ID', '21938068'))  # Your API ID
-    API_HASH = getenv('API_HASH', 'c18fd98f3e58484df0aecd95a3d5a6a9')  # Your API Hash
-    BOT_TOKEN = getenv('BOT_TOKEN', '5972634422:AAG2Q7ozpB4tKCVTCTJcFT7504GpeUYNTgM')  # Your bot token
-    name = getenv('SESSION_NAME', 'filetolinkbot')
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', '21938068'))
+    API_HASH = str(getenv('API_HASH', 'c18fd98f3e58484df0aecd95a3d5a6a9'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5972634422:AAG2Q7ozpB4tKCVTCTJcFT7504GpeUYNTgM'))
+    name = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    PORT = environ.get("PORT", "65535")  # Set the web server port to 65535
-
-    # Telegram configuration
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001730461072'))  # Your Bin Channel
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5787509903").split())  # List of owner IDs
-    OWNER_USERNAME = getenv('OWNER_USERNAME', 'Madhuri_niranjan')  # Owner's username
-
-    # Network settings
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001730461072'))
+    PORT = int(getenv('PORT', 65535))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '181.214.152.133'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5787509903").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-
-    # Heroku configuration
-    ON_HEROKU = 'DYNO' in environ
-    APP_NAME = getenv('APP_NAME')
-
-    # Web server configuration
-    BIND_ADDRESS = getenv('BIND_ADDRESS', '181.214.152.133')
-    FQDN = getenv('FQDN', 'srv31221116.ultasrv.net:65535') if not ON_HEROKU or getenv('FQDN', 'srv31221116.ultasrv.net:65535') else APP_NAME + '.herokuapp.com'
-
-    # SSL configuration
-    HAS_SSL = bool(getenv('HAS_SSL', False))
-    URL = "https://{}/".format(FQDN) if HAS_SSL else "http://{}/".format(FQDN)
-
-    # Database configuration
-    DATABASE_URL = getenv('DATABASE_URL', 'mongodb+srv://Filetolink:yuvraj178@cluster0.focy2ua.mongodb.net/?retryWrites=true&w=majority')  # Your database connection URL
-
-    # Updates channel (optional)
-    UPDATES_CHANNEL = getenv('UPDATES_CHANNEL', 'Infinity_XBotz')  # Your updates channel username
-
-    # Banned channels (if applicable)
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))  # List of banned channels
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Madhuri_niranjan'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
     
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', '181.214.152.133:65535')) if not ON_HEROKU or getenv('FQDN', '181.214.152.133:65535') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Filetolink:yuvraj178@cluster0.focy2ua.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Infinity_XBotz'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
